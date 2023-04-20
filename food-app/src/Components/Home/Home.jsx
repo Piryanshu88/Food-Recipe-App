3c2eef9b31db2018a35711b693d42fda10d237cd
Piryanshu88 <deepubisht2004@gmail.com>
2023-04-20 22:55:10 +0530

    adding basic routes

---
+++ food-app/src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Footer } from "../Footer/Footer";
+
+export const Home = () => {
+  return (
+    <div>
+      <div>Home</div>
+      <Footer />
+    </div>
+  );
+};
---
+++ food-app/src/Pages/Home/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Home } from "../../Components/Home/Home";
+
+export const HomePage = () => {
+  return <Home />;
+};
