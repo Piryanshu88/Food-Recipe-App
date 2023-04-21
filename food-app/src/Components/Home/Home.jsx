86f71a457eeeef93513701bb9e0e881d1ff83c96
Piryanshu88 <deepubisht2004@gmail.com>
2023-04-21 18:52:17 +0530

    adding basic ui for landing page

--- food-app/src/Components/Home/Home.jsx
+++ food-app/src/Components/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
-import img from "../../Assets/recipeHome.jpg";
+import ico from "../../Assets/daco.png";
+import img from "../../Assets/recipeHome.png";
 import styles from "./Home.module.css";
-import { Heading, Image, Text } from "@chakra-ui/react";
+import { Flex, Heading, Image, Text } from "@chakra-ui/react";
 export const Home = () => {
   return (
     <div>
@@ -14,13 +14,13 @@ export const Home = () => {
             fontSize={{ base: "3xl", sm: "4xl", lg: "7xl" }}
           >
             From Kitchen to Table :
-            <Text as={"span"} color={"red.400"} fontWeight="800">
+            <Text as={"span"} color={"green.400"} fontWeight="800">
               {" "}
               Cook Healthy and Delicious Meals with us.
             </Text>
           </Heading>
         </div>
-        <Image src={img} alt="" width={"50%"} />
+        <Image src={img} alt="" />
       </div>
     </div>
   );
