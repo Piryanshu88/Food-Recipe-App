72aab85ffb86a0deb046f9d3a79e36112338400d
Piryanshu88 <deepubisht2004@gmail.com>
2023-04-21 18:32:08 +0530

    adding a new font style :tada:

--- food-app/src/Components/Home/Home.jsx
+++ food-app/src/Components/Home/Home.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Footer } from "../Footer/Footer";
 
+import img from "../../Assets/recipeHome.jpg";
+import styles from "./Home.module.css";
+import { Heading, Image, Text } from "@chakra-ui/react";
 export const Home = () => {
   return (
     <div>
-      <div>Home</div>
-      <Footer />
+      <div className={styles.home_section}>
+        <div className={styles.home_section_text}>
+          <Heading
+            lineHeight={1.1}
+            fontWeight={800}
+            fontSize={{ base: "3xl", sm: "4xl", lg: "7xl" }}
+          >
+            From Kitchen to Table :
+            <Text as={"span"} color={"red.400"} fontWeight="800">
+              {" "}
+              Cook Healthy and Delicious Meals with us.
+            </Text>
+          </Heading>
+        </div>
+        <Image src={img} alt="" width={"50%"} />
+      </div>
     </div>
   );
 };
